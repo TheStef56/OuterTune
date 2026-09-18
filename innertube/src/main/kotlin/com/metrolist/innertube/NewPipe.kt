9d531360927c025c54b16e23352c28179a8dfb5a
TheStef56 <stefanosanasi2017@gmail.com>
2026-09-18 11:19:30 +0200

    working innertubex. now it plays again from yt.

--- innertube/src/main/kotlin/com/metrolist/innertube/NewPipe.kt
+++ innertube/src/main/kotlin/com/metrolist/innertube/NewPipe.kt
@@ -1,5 +1,6 @@
 package com.metrolist.innertube
 
+import android.util.Log
 import com.metrolist.innertube.models.YouTubeClient
 import com.metrolist.innertube.models.response.PlayerResponse
 import io.ktor.http.URLBuilder
